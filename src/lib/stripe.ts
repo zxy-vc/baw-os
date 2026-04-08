d9bd30770bfcd934b062d157a0378af7a1e24982
Fran Durán <fran@zxy.vc>
2026-04-08 11:09:07 -0600

    fix: stripe apiVersion to match installed package type

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import { loadStripe } from '@stripe/stripe-js'
 
 // Server-side Stripe client
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2024-04-10',
   typescript: true,
 })
 
